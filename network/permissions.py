4b62f1c416160d66c694970c39851b4924c0d69c
adminnn <geojesi1@mail.ru>
2025-10-01 13:28:56 +0400

    add new tests and README.md

--- network/permissions.py
+++ network/permissions.py
@@ -6,11 +6,7 @@ class IsActiveStaff(BasePermission):
     Доступ к API только для аутентифицированных пользователей,
     у которых is_active=True и is_staff=True.
     """
+
     def has_permission(self, request, view):
         user = request.user
-        return bool(
-            user
-            and user.is_authenticated
-            and user.is_active
-            and user.is_staff
-        )
+        return bool(user and user.is_authenticated and user.is_active and user.is_staff)
